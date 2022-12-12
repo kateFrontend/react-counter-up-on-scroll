3b490e699c4d5ead4d304fe798ba82a9cffd0fbc
kateFrontend <ecdanilova@gmail.com>
2022-12-12 17:42:39 -0500

    update css

--- src/App.js
+++ src/App.js
@@ -1,135 +1,15 @@
-import { isVisible } from "@testing-library/user-event/dist/utils";
-import React, { useState } from "react";
-import CountUp from 'react-countup';
-import ReactVisibilitySensor from "react-visibility-sensor";
-import './App.css'
+import React from "react";
+import "./App.css";
+import Counter from "./components/Counter";
 
-function App({ className, ...rest }) {
-  const [viewPortEntered, setViewPortEntered] = useState(false);
-
+function App() {
   return (
     <>
-      <section className="counter">
-        <div className="title">
-        <h1>our achievements</h1>
-        </div>
-
-        <div className="counter-row">
-
-          <div className="counter-column">
-            <strong data-number="310">
-              <CountUp {...rest} start={viewPortEntered ? null : 0} end={310}>
-                {({ countUpRef }) => {
-                  return (
-                    <ReactVisibilitySensor
-                      active={!viewPortEntered}
-                      onChange={
-                        (isVisible => {
-                          if(isVisible) {
-                            setViewPortEntered(true);
-                          }
-                        })
-                      }
-                      delayedCall
-                    >
-                      <span className="number" ref={countUpRef} />
-                    </ReactVisibilitySensor>
-                  );
-                }}
-              </CountUp>
-            </strong>
-            <span>
-              Some text <br />
-              Goes here
-            </span>
-          </div>
-
-          <div className="counter-column">
-            <strong data-number="1250">
-              <CountUp {...rest} start={viewPortEntered ? null : 0} end={1250}>
-                {({ countUpRef }) => {
-                  return (
-                    <ReactVisibilitySensor
-                      active={!viewPortEntered}
-                      onChange={
-                        (isVisible => {
-                          if(isVisible) {
-                            setViewPortEntered(true);
-                          }
-                        })
-                      }
-                      delayedCall
-                    >
-                      <span className="number" ref={countUpRef} />
-                    </ReactVisibilitySensor>
-                  );
-                }}
-              </CountUp>
-            </strong>
-            <span>
-            Some text <br />
-              Goes here
-            </span>
-          </div>
-
-          <div className="counter-column">
-            <strong data-number="125">
-              <CountUp {...rest} start={viewPortEntered ? null : 0} end={125}>
-                {({ countUpRef }) => {
-                  return (
-                    <ReactVisibilitySensor
-                      active={!viewPortEntered}
-                      onChange={
-                        (isVisible => {
-                          if(isVisible) {
-                            setViewPortEntered(true);
-                          }
-                        })
-                      }
-                      delayedCall
-                    >
-                      <span className="number" ref={countUpRef} />
-                    </ReactVisibilitySensor>
-                  );
-                }}
-              </CountUp>
-            </strong>
-            <span>
-            Some text <br />
-              Goes here
-            </span>
-          </div>
-          
-          <div className="counter-column">
-            <strong data-number="24">
-              <CountUp {...rest} start={viewPortEntered ? null : 0} end={24}>
-                {({ countUpRef }) => {
-                  return (
-                    <ReactVisibilitySensor
-                      active={!viewPortEntered}
-                      onChange={
-                        (isVisible => {
-                          if(isVisible) {
-                            setViewPortEntered(true);
-                          }
-                        })
-                      }
-                      delayedCall
-                    >
-                      <span className="number" ref={countUpRef} />
-                    </ReactVisibilitySensor>
-                  );
-                }}
-              </CountUp>
-            </strong>
-            <span>
-            Some text <br />
-              Goes here
-            </span>
-          </div>
-
-        </div>
-      </section>
+      <div className="hero">
+        <h1>Scroll down</h1>
+      </div>
+      <Counter />
+      <div className="footer"></div>
     </>
   );
 }
