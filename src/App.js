9a1d396cda501c3976b7e36fa6dcea72aa52f4b0
kateFrontend <ecdanilova@gmail.com>
2022-12-12 14:20:52 -0500

    setup counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { isVisible } from "@testing-library/user-event/dist/utils";
+import React, { useState } from "react";
+import CountUp from "react-countup/build/CountUp";
+import ReactVisibilitySensor from "react-visibility-sensor";
 
-function App() {
+function App({ className, ...rest }) {
+
+const [viewPortEntered, setViewPortEntered] = useState(false)
+
   return (
-    <div>App</div>
-  )
+    <>
+      <section className="counter">
+        <div className="counter-row">
+          <div className="counter-column">
+            <strong data-number='305'>
+              <CountUp {...rest} start = {  viewPortEntered ? null : 0 } end={305} >
+                {({ countUpRef }) => {
+                    return(
+                    <ReactVisibilitySensor
+                    active = {!viewPortEntered}
+                    onChange = {isVisible = {
+                      if(isVisible){
+                        setViewPortEntered(true)
+                      }
+                    }}
+                    delayedCall
+                    >
+                      <span className="number" ref={countUpRef}/>
+                    </ReactVisibilitySensor>
+                    )
+                }}
+              </CountUp>
+            </strong>
+            <span>On Going <br/>Projects</span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
